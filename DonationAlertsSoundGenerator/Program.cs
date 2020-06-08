a25ddd73f4bb8186837a69bd9042854f9d707775
Gnome <alexeynshah@gmail.com>
2020-06-08 20:20:55 +0300

    Parser

---
+++ DonationAlertsSoundGenerator/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Web;
+
+namespace DonationAlertsSoundGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            string url;
+            string filename;
+
+            string randomNewDirectory = "DonationAlertSounds" + DateTime.Now.ToString("dd.MM.yyyyInhh-mm-ss");
+            string currentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+
+            Directory.CreateDirectory(@currentDirectory + @randomNewDirectory);
+
+            WebClient client = new WebClient();
+            HttpWebRequest webRequest;
+            HttpWebResponse response;
+
+            while (true)
+            {
+
+                url = "https://static.donationalerts.ru/audiodonations/";
+                string tempurl = "6" + rnd.Next(4, 6).ToString() + rnd.Next(111, 999).ToString();
+                url += tempurl + '/' + tempurl + rnd.Next(111, 999).ToString();
+                url += ".wav";
+
+                filename = url.Split('/').Last();
+
+                webRequest = (HttpWebRequest)WebRequest.Create(new Uri(url));
+                response = (HttpWebResponse)webRequest.GetResponse();
+                if ((int)response.StatusCode == 200)
+                {
+                    Console.WriteLine(url + " - Valid");
+                    client.DownloadFile(new Uri(url), currentDirectory + randomNewDirectory + "\\" + filename);
+                }
+                else
+                {
+                    Console.WriteLine(url + " - Invalid");
+                }
+            }
+        }
+    }
+}
---
+++ DonationAlertsSoundGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DonationAlertsSoundGenerator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Gnomee1337")]
+[assembly: AssemblyProduct("DonationAlertsSoundGenerator")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("250ec50f-6b78-477e-8599-989d59a932c7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
